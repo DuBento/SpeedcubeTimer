7c4c9c6e02147a38f1fa79bbe8c50e1f2dee5956
DuBento <duarte.bento@tecnico.ulisboa.pt>
2022-01-16 16:28:20 +0000

    Inital commit, basic label timer

---
+++ main.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <gtk/gtk.h>
+#include "timer.h"
+
+GtkWidget *labTime;
+
+void _updateTimerLabel(Time, gboolean);
+
+void updateTimerLabel (Time time) {
+	_updateTimerLabel(time, FALSE);
+}
+
+void updateTimerLabelFinal (Time time) {
+	_updateTimerLabel(time, TRUE);
+}
+
+void _updateTimerLabel (Time time, gboolean bFinal) {
+	char buffer[16];
+	if (!bFinal) time.msecs = time.msecs/10; 
+
+	if (time.min != 0)
+		sprintf(buffer, "%d:%d.%d", time.min, time.secs, time.msecs);
+	else
+		sprintf(buffer, "%d.%d", time.secs, time.msecs);
+
+	gtk_label_set_text (GTK_LABEL (labTime), buffer);
+}
+
+gboolean keypressHandler (
+	GtkEventControllerKey* self,
+	guint keyval,
+	guint keycode,
+	GdkModifierType state,
+	gpointer user_data)
+{
+    if (keyval == GDK_KEY_space){
+		toggleTimer();
+        return TRUE;
+    }
+    return FALSE;
+}
+
+static void
+appActivate (GApplication *app, gpointer user_data) {
+	GtkWidget *win;
+
+	win = gtk_application_window_new (GTK_APPLICATION (app));
+	gtk_window_set_title (GTK_WINDOW (win), "Speedcube Timer");
+	gtk_window_set_default_size (GTK_WINDOW (win), 400, 300);
+
+	/* Label */
+	labTime = gtk_label_new ("0.0");
+	gtk_window_set_child (GTK_WINDOW (win), labTime);
+
+	/* Keyboard event */
+	GtkEventController *keyController = gtk_event_controller_key_new();
+	g_signal_connect (G_OBJECT (keyController), "key-pressed",
+        G_CALLBACK (keypressHandler), NULL);
+	gtk_widget_add_controller(win, keyController);
+
+	gtk_widget_show (win);
+}
+
+int
+main (int argc, char **argv) {
+	GtkApplication *app;
+	int stat;
+
+	app = gtk_application_new ("speedcube.gtk.timer", G_APPLICATION_FLAGS_NONE);
+	g_signal_connect (app, "activate", G_CALLBACK (appActivate), NULL);
+	stat = g_application_run (G_APPLICATION (app), argc, argv);
+	g_object_unref (app);
+	return stat;
+}
---
+++ timer.c
@@ -0,0 +1,65 @@
+/**
+ * @file timer.c
+ * @author DuBento (github.com/DuBento)
+ * @brief Simple timer implementation to be displayed
+ * @version 0.1
+ * @date 2022-01-16
+ * 
+ * Chose to use Gtimer for more precision;
+ * Timeout used to display new time;
+ */
+
+#include "timer.h"
+
+static int bTimerRunning = FALSE;
+static GTimer* timer = NULL;
+/* Elapsed Time in 1/100 ths of second */
+// static double elapsedTime = 0;
+
+void updateTimerLabel (Time);
+void updateTimerLabelFinal (Time);
+
+static gboolean timerCallback (gpointer data) {
+    Time time;
+    gdouble elapsed;
+    
+    if (timer) {
+        elapsed = g_timer_elapsed(timer, NULL);
+
+        time.min = elapsed / 60;
+        time.secs = (int) elapsed % 60;
+        time.msecs = (int) (elapsed * 100) % 100;
+
+        // g_print("%d, %d, %d\n", time.min, time.secs, time.msecs);
+    }
+
+    if (bTimerRunning) {
+        updateTimerLabel(time);    
+        return G_SOURCE_CONTINUE;
+    }
+    else {
+        updateTimerLabelFinal(time);
+        g_timer_destroy(timer);
+        /* Stop timeout timer */
+        return G_SOURCE_REMOVE;
+    }
+}
+
+void startTimer () {
+    /* Timer starts */
+    timer = g_timer_new();
+    bTimerRunning = TRUE;
+
+    /* Callback every 100ms */
+    g_timeout_add(100, timerCallback, NULL);
+}
+
+void stopTimer () {
+    g_timer_stop(timer);
+    bTimerRunning = FALSE;
+}
+
+gboolean toggleTimer () {
+    !bTimerRunning ? startTimer() : stopTimer();
+    return bTimerRunning;
+}
